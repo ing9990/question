677d4f08c857d4c5835b0f05174704023c991c12
ing9990 <gimgau0218@naver.com>
2023-03-31 18:29:54 +0900

    develop: 유저의 정보를 가져올 서비스를 추상화 시킴

--- src/main/java/com/question/infra/in/resolver/CurrentUserResolver.java
+++ src/main/java/com/question/infra/in/resolver/CurrentUserResolver.java
@@ -1,8 +1,10 @@
 package com.question.infra.in.resolver;
 
+import com.question.auth.application.AuthService;
 import com.question.infra.in.aop.CurrentUser.CurrentUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -12,6 +14,10 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 @Component
 @RequiredArgsConstructor
 public class CurrentUserResolver implements HandlerMethodArgumentResolver {
+
+    private final AuthService authService;
+
+    public static final String AUTHORIZTION_HEADER_NAME = HttpHeaders.AUTHORIZATION;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -24,7 +30,7 @@ public class CurrentUserResolver implements HandlerMethodArgumentResolver {
                                   NativeWebRequest webRequest,
                                   WebDataBinderFactory binderFactory) throws Exception {
 
-
+        String authorization = webRequest.getHeader(AUTHORIZTION_HEADER_NAME);
 
 
         return null;
