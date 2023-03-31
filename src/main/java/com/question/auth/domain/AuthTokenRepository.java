a269efa9c7a0caa81c0d06a00a951e6d3a546d45
ing9990 <gimgau0218@naver.com>
2023-03-31 18:30:10 +0900

    develop: Delete AuthTokenRepository

--- src/main/java/com/question/auth/domain/AuthTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.question.auth.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthTokenRepository extends JpaRepository<AuthToken, Long> {
-}
