82b597699e8dbb21075dfb678e7107f4d9212358
ing9990 <gimgau0218@naver.com>
2023-04-03 05:27:47 +0900

    develop: 질문 ID 로 질문을 조회하는 API 생성

--- src/main/java/com/question/question/presentation/QuestionApi.java
+++ src/main/java/com/question/question/presentation/QuestionApi.java
@@ -2,13 +2,11 @@ package com.question.question.presentation;
 
 import com.question.question.application.QuestionService;
 import com.question.question.io.request.CreateQuestionRequest;
+import com.question.question.io.response.QuestionResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -18,6 +16,14 @@ import javax.validation.Valid;
 public class QuestionApi {
 
     private final QuestionService questionService;
+
+    @GetMapping("/{questionId}")
+    public ResponseEntity<QuestionResponse> getQuestion(
+            @PathVariable Long questionId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(questionService.getQuestion(questionId));
+    }
 
     @PostMapping
         //@LoginRequired
