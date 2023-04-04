88abc1140d1c438722e4b2659c815795464012d6
ing9990 <gimgau0218@naver.com>
2023-04-04 17:33:18 +0900

    refactor: 패스워드 인코더는 새로운 구성정보에 포함하도록 변경

--- src/main/java/com/question/infra/in/SecurityConfig.java
+++ src/main/java/com/question/infra/in/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.question.infra.in;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
 
     @Override
--- src/main/java/com/question/infra/in/WebConfig.java
+++ src/main/java/com/question/infra/in/WebConfig.java
@@ -3,10 +3,7 @@ package com.question.infra.in;
 import com.question.infra.in.resolver.CurrentUserResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,11 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
     private List<String> allowOriginMethods;
 
     private final CurrentUserResolver currentUserArgumentResovler;
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
