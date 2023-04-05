b26c34901db91143d1c08442bcdd323aba4f0567
ing9990 <gimgau0218@naver.com>
2023-04-06 08:51:30 +0900

    test: Question 도메인 수정 부분을 단위테스트에 반영
    - 질문의 본문이 5글자 미만이라면 IAE를 던진다.
    - 질문의 제목이 3글자 미만이라면 IAE를 던진다.
    - 인자의 User가 작성자가 맞다면 True를 반환한다.
    - 질문자를 찾을 수 없다면 IAE를 던진다.

--- src/test/java/com/question/question/domain/QuestionTest.java
+++ src/test/java/com/question/question/domain/QuestionTest.java
@@ -5,8 +5,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class QuestionTest {
+
+    private Question given(
+            String title,
+            String detail,
+            User author
+    ) {
+        return new Question(title, detail, author);
+    }
 
     @DisplayName("Question 객체 생성 테스트")
     @Test
@@ -28,5 +37,38 @@ public class QuestionTest {
         assertThat(question.getTitle()).isEqualTo(title);
         assertThat(question.getDetail()).isEqualTo(detail);
         assertThat(question.getAuthor()).isEqualTo(author);
+    }
+
+
+    @DisplayName("질문자를 찾을 수 없다면 IllegalArgumentExcpetion을 던진다.")
+    @Test
+    void testAuthorIsNull() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> given("test-title", "test-detail", null));
+    }
+
+
+    @DisplayName("질문의 제목이 3글자 미만이라면 IllegalArgumentExcpetion을 던진다.")
+    @Test
+    void testQuestionTitleLessThan3() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> given("i", "test-detail", null));
+    }
+
+    @DisplayName("질문의 본문이 5글자 미만이라면 IllegalArgumentExcpetion을 던진다.")
+    @Test
+    void testQuestionDetailLessThan5() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> given("test-title", "i", null));
+    }
+
+    @DisplayName("질문의 작성자가 맞다면 True를 반환한다.")
+    @Test
+    void returnTrueIfSameAuthor() {
+        //given
+        User user = new User("username", "email@email.com", "password", "");
+        Question question = given("test-title", "test-detail", user);
+
+        assertThat(question.isAuthor(user.getUserId())).isTrue();
     }
 }
