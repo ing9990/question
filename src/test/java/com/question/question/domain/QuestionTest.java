bfac116f37a5f3590bdeea7cdba6abde9abcaaa2
ing9990 <gimgau0218@naver.com>
2023-04-06 09:14:53 +0900

    test: 답변 추가 테스트 코드 작성
    - 답변은 질문에 포함될 수 있고 내부 내용과 작성자는 변경되지 않아야 한다.

--- src/test/java/com/question/question/domain/QuestionTest.java
+++ src/test/java/com/question/question/domain/QuestionTest.java
@@ -1,6 +1,8 @@
 package com.question.question.domain;
 
+import com.question.answer.domain.Answer;
 import com.question.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +11,43 @@ import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 
 public class QuestionTest {
 
-    private Question given(
-            String title,
-            String detail,
-            User author
-    ) {
-        return new Question(title, detail, author);
+
+    private String questionTitle = "블록체인 기업 면접 질문";
+    private String questionDetail = "블록체인 기술의 '영지식증명'이 뭔가요?";
+
+    private User author;
+    private Question question;
+
+    @BeforeEach
+    public void each() {
+        author = new User("blockchain",
+                "blockchain@gmail.com",
+                "testpassword",
+                "https://testimage/image.png");
+
+        question = new Question(questionTitle,
+                questionDetail,
+                author);
+    }
+
+    @DisplayName("답변은 질문에 포함될 수 있고 내부 내용과 작성자는 변경되지 않아야한다.")
+    @Test
+    void addAnswer() {
+        Answer answer = new Answer(question,
+                author,
+                "영지식증명에 대해 설명드립니다.",
+                "영지식 증명(ZKP)은 증명자가 자신의 지식을 공개하지 않고 그 사실을 검증자에게 증명하는 시스템입니다.");
+
+        question.addAnswer(answer);
+
+        assertThat(question.getAnswers().size()).isEqualTo(1);
+        assertThat(question.getAnswers().stream().findFirst().isPresent()).isTrue();
+
+        Answer firstAnswer = question.getAnswers().get(0);
+
+        assertThat(firstAnswer.getAnswerer()).isEqualTo(author);
+        assertThat(firstAnswer.getQuestion().getTitle()).isEqualTo(questionTitle);
+        assertThat(firstAnswer.getQuestion().getDetail()).isEqualTo(questionDetail);
     }
 
     @DisplayName("Question 객체 생성 테스트")
@@ -70,5 +103,13 @@ public class QuestionTest {
         Question question = given("test-title", "test-detail", user);
 
         assertThat(question.isAuthor(user.getUserId())).isTrue();
+    }
+
+    private Question given(
+            String title,
+            String detail,
+            User author
+    ) {
+        return new Question(title, detail, author);
     }
 }
